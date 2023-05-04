211b17e9d9e65ddd29e7a5be89b7e7d5849f15aa
PrincessRuth90 <marcusruth86@gmail.com>
2023-05-04 23:40:00 +0000

    Fix task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -67,4 +67,4 @@ def authenticate_user():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port debug=True)
