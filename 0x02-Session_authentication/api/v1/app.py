4ce80cd05cbc4937d605e3622b6090a54c6e0e8a
PrincessRuth90 <marcusruth86@gmail.com>
2023-05-05 02:10:08 +0000

    Task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,9 +12,8 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
-
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,12 +24,6 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-if auth_type == 'session_auth':
-    auth = SessionAuth()
-if auth_type == 'session_exp_auth':
-    auth = SessionExpAuth()
-if auth_type == 'session_db_auth':
-    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -67,14 +60,12 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            auth_session = auth.session_cookie(request)
             user = auth.current_user(request)
-            if auth_header is None and \ 
-               auth_session is None:
+            if auth_header is None:
+                auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
-            request.current_user = user
 
 
 if __name__ == "__main__":
