bb0983d03affa1a5b1557068f4f572833377bb9e
PrincessRuth90 <marcusruth86@gmail.com>
2023-05-05 01:53:06 +0000

    Fix task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""Route module for the API.
 """
-import os
+Route module for the API
+"""
 from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -33,7 +35,7 @@ if auth_type == 'session_db_auth':
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
@@ -58,15 +60,17 @@ def authenticate_user():
     """
     if auth:
         excluded_paths = [
-            "/api/v1/status/",
-            "/api/v1/unauthorized/",
-            "/api/v1/forbidden/",
-            "/api/v1/auth_session/login/",
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            auth_session = auth.session_cookie(request)
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
+            if auth_header is None and \ 
+               auth_session is None:
                 abort(401)
             if user is None:
                 abort(403)
